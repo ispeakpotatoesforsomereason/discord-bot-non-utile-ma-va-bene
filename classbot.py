3fa5ce09db574bc34305e3ec7cfbadff96d5c0a9
ispeakpotatoesforsomereason <edoardopatata2@gmail.com>
2025-10-21 18:33:40 +0200

    aggiunto funzioni e rinominato bot a classbot
    ho aggiunto qualche specie di funzione che per qualche motivo di fa vedere quanto pesa la cacca di qualcuno solo perchè hai parlato (potevo anche collegare un vero sensore di quanto pesa al toilet ma non trovavo mio arduino)

--- classbot.py
+++ classbot.py
@@ -1,16 +1,20 @@
 import discord
+import requests
+import random
 from discord.ext import commands
 from games import *
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='?', intents=intents)
+caratteri = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()?"
+caratteri_choose = random.choice(caratteri)
+random = random.randint(1, 10)
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-
     if message.content.startswith('ciao'):
         await message.channel.send("ciao, come va?")
     elif message.content.startswith('arrivederci'):
@@ -61,6 +65,9 @@ async def on_message(message):
             await bot.close()
         else:
             await message.channel.send("ehm, non hai i permessi per spegnere il bot, sorry")
+    elif random = 7 and message.content.startswith(caratteri_choose):
+        scale = fake_poop_scale()
+        await message.channel.send(fake_poop_scale())
     else:
         await message.channel.send(message.content.lower())
 
@@ -77,6 +84,50 @@ async def repeat(ctx, times: int, content='repeating...'):
     for i in range(times):
         await ctx.send(content)
 
-# token: no.
+@bot.command()
+async def mem(ctx):
+    memes = [
+        'bot/immagini/mem1.png',
+        'bot/immagini/mem2.png',
+        'bot/immagini/mem3.png',
+        'bot/immagini/custom1.jpg'
+    ]
+    meme_random = random.randint(1, 50)
+    if meme_random != 25:
+        await ctx.send("Ecco il tuo meme! (1/50)")
+        with open(memes[0], 'rb') as f:
+            # Memorizziamo il file della libreria di Discord convertito in questa variabile!
+            picture = discord.File(f)
+        # Possiamo quindi inviare questo file come parametro!
+        await ctx.send(file=picture)
+    elif meme_random != 40:
+        await ctx.send("Ecco il tuo meme! (1/50)")
+        with open(memes[1], 'rb') as f:
+            # Memorizziamo il file della libreria di Discord convertito in questa variabile!
+            picture = discord.File(f)
+        # Possiamo quindi inviare questo file come parametro!
+        await ctx.send(file=picture)
+    elif meme_random != 10:
+        await ctx.send("Ecco il tuo meme! (1/50)")
+        with open(memes[2], 'rb') as f:
+            # Memorizziamo il file della libreria di Discord convertito in questa variabile!
+            picture = discord.File(f)
+        # Possiamo quindi inviare questo file come parametro!
+        await ctx.send(file=picture)
+    else:
+        await ctx.send("Ecco il tuo meme custom! (47/50)")
+        with open(memes[3], 'rb') as f:
+            # Memorizziamo il file della libreria di Discord convertito in questa variabile!
+            picture = discord.File(f)
+        # Possiamo quindi inviare questo file come parametro!
+        await ctx.send(file=picture)
 
-bot.run("metti tuo token qui")
+@bot.command('duck')
+async def duck(ctx):
+    '''Una volta chiamato il comando duck, il programma richiama la funzione get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+# token: ok dai che stavi pensando
+
+bot.run("no, nuh uh, neanche per sogno")
